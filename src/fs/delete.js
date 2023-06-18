8ff6abab1f012fefec68f06f5337c1f9f7354149
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2023-06-18 21:28:37 +0400

    Implemented function that deletes file.

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,32 @@
+// delete.js - implement function that deletes file fileToRemove.txt
+// (if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)
+
+import fs from 'fs';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const filesDir = path.join(__dirname, 'files');
+
+    const removedFile = path.join(filesDir, 'fileToRemove.txt');
+    fs.access(removedFile, fs.constants.F_OK, (err) => {
+        if (err) {
+            throw new Error("FS operation failed");
+        }
+    });
+
+    fs.unlink(removedFile, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+
 };
 
 await remove();
