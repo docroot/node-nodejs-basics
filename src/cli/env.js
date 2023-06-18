cdbbbea7c21e656dc53f826f7c3f2f05f1d1fa23
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2023-06-19 01:20:58 +0400

    Implemented function that parses environment variables with prefix RSS_ and prints them to the console.

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+// env.js - implement function that parses environment variables with prefix RSS_
+// and prints them to the console in the format "RSS_name1=value1; RSS_name2=value2"
+
 const parseEnv = () => {
-    // Write your code here 
+    const re = /^RSS_/;
+    const rssvars = [];
+    Object.keys(process.env).forEach((key) => {
+        if (re.test(key)) {
+            rssvars.push(`${key}=${process.env[key]}`);
+        }
+    });
+    console.log(rssvars.join('; '));
 };
 
 parseEnv();
