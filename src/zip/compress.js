a4c5605e3197f46245da62a00be08639791450cb
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2023-06-19 22:05:25 +0400

    Implemented functions that compresses/decompresses file.

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+// implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const fileName = path.join(__dirname, 'files/fileToCompress.txt');
+    const archName = path.join(__dirname, 'files/archive.gz');
+
+    const inputStream = fs.createReadStream(fileName);
+    const gzipStream = zlib.createGzip();
+    const outputStream = fs.createWriteStream(archName);
+
+    inputStream.pipe(gzipStream).pipe(outputStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+// implement function that decompresses archive.gz back to the fileToCompress.txt
+// with same content as before compression using zlib and Streams API
+
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const archName = path.join(__dirname, 'files/archive.gz');
+    const fileName = path.join(__dirname, 'files/fileToCompress.txt');
+
+    const inputStream = fs.createReadStream(archName);
+    const gzipStream = zlib.createGunzip();
+    const outputStream = fs.createWriteStream(fileName);
+
+    inputStream.pipe(gzipStream).pipe(outputStream);
 };
 
 await decompress();
