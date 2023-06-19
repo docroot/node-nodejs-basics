e27094f25b7b1fdc96c364f9d45b64a53cc0386d
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2023-06-20 02:54:10 +0400

    fix: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -12,33 +12,20 @@ const __dirname = dirname(__filename);
 
 const create = async () => {
     const filesDir = path.join(__dirname, 'files');
-    fs.stat(filesDir, (err, stats) => {
-        if (err && err.code === 'ENOENT') {
-            throw new Error(filesDir + " does not exist.");
-        }
-        if (!stats.isDirectory()) {
-            throw new Error(filesDir + " is not a directory.");
-        }
-    });
-    fs.access(filesDir, fs.constants.W_OK, (err) => {
-        if (err) {
-            throw new Error(filesDir + " is not writable.");
-        }
-    });
-
     const freshFile = path.join(filesDir, 'fresh.txt');
+
     fs.access(freshFile, fs.constants.F_OK, (err) => {
         if (err === null) {
             throw new Error("FS operation failed");
         }
+        const freshContent = 'I am fresh and young';
+        fs.writeFile(freshFile, freshContent, (err) => {
+            if (err) {
+                throw err;
+            }
+        });
     });
 
-    const freshContent = 'I am fresh and young';
-    fs.writeFile(freshFile, freshContent, (err) => {
-        if (err) {
-            throw err;
-        }
-    });
 };
 
 await create();
