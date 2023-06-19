48a498235e3eb4f5bbdb01c627e05149f6bb0eb4
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2023-06-20 01:48:34 +0400

    Work with child process.

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+// implement function spawnChildProcess that receives array of arguments args and creates child process from file
+import { spawn } from 'child_process';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const nodejs = process.argv[0];
+    const scriptFile = path.join(__dirname, 'files', 'script.js');
+    args.unshift(scriptFile);
+
+    const childProcess = spawn(nodejs, args);
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['--opt1', 'val1', '--opt2', 'val2']);
