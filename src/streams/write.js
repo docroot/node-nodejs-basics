4fabc6d194e454f79d51dec061ef9679c9c91741
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2023-06-19 21:11:04 +0400

    Implemeted functions that work withj streams.

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+// implement function that writes process.stdin data into file fileToWrite.txt
+// content using Writable Stream
+import fs from 'fs';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const fileName = path.join(__dirname, 'files/fileToWrite.txt');
+
+    const outputStream = fs.createWriteStream(fileName);
+
+    process.stdin.pipe(outputStream);
 };
 
 await write();
