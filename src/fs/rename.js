a935f1b45bb653a159568c8933c67f46643c5f00
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2023-06-20 03:01:59 +0400

    fix: rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -14,25 +14,22 @@ const __dirname = dirname(__filename);
 
 const rename = async () => {
     const filesDir = path.join(__dirname, 'files');
-
     const wrongFile = path.join(filesDir, 'wrongFilename.txt');
+    const properFile = path.join(filesDir, 'properFilename.md');
     fs.access(wrongFile, fs.constants.F_OK, (err) => {
         if (err) {
             throw new Error("FS operation failed");
         }
-    });
-
-    const properFile = path.join(filesDir, 'properFilename.md');
-    fs.access(properFile, fs.constants.F_OK, (err) => {
-        if (err === null) {
-            throw new Error("FS operation failed");
-        }
-    });
-
-    fs.rename(wrongFile, properFile, (err) => {
-        if (err) {
-            throw new Error("FS operation failed");
-        }
+        fs.access(properFile, fs.constants.F_OK, (err) => {
+            if (err === null) {
+                throw new Error("FS operation failed");
+            }
+            fs.rename(wrongFile, properFile, (err) => {
+                if (err) {
+                    throw new Error("FS operation failed");
+                }
+            });
+        });
     });
 };
 
