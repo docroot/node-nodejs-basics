d7e93db698c7a82f584194391dd01be8534c6c76
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2023-06-20 02:51:27 +0400

    fix: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -13,41 +13,30 @@ const __dirname = dirname(__filename);
 
 const copy = async () => {
     const filesDir = path.join(__dirname, 'files');
+    const filesCopyDir = path.join(__dirname, 'files_copy');
     fs.stat(filesDir, (err, stats) => {
         if (err && err.code === 'ENOENT') {
             throw new Error("FS operation failed");
         }
-        if (!stats.isDirectory()) {
-            throw new Error(filesDir + " is not a directory.");
-        }
-    });
-    fs.access(filesDir, fs.constants.R_OK, (err) => {
-        if (err) {
-            throw new Error(filesDir + " is not readable.");
-        }
-    });
-
-    const filesCopyDir = path.join(__dirname, 'files_copy');
-    fs.access(filesCopyDir, fs.constants.F_OK, (err) => {
-        if (err === null) {
-            throw new Error("FS operation failed");
-        }
-    });
-
-    fs.mkdir(filesCopyDir, { recursive: true }, (err) => {
-        if (err) throw err;
-    });
-
-    fs.readdir(filesDir, (err, files) => {
-        if (err) {
-            console.error('Error reading directory:', err);
-            return;
-        }
-        files.forEach((file) => {
-            const srcPath = path.join(filesDir, file);
-            const dstPath = path.join(filesCopyDir, file);
-            fs.copyFile(srcPath, dstPath, (err) => {
+        fs.access(filesCopyDir, fs.constants.F_OK, (err) => {
+            if (err === null) {
+                throw new Error("FS operation failed");
+            }
+            fs.mkdir(filesCopyDir, { recursive: true }, (err) => {
                 if (err) throw err;
+                fs.readdir(filesDir, (err, files) => {
+                    if (err) {
+                        console.error('Error reading directory:', err);
+                        return;
+                    }
+                    files.forEach((file) => {
+                        const srcPath = path.join(filesDir, file);
+                        const dstPath = path.join(filesCopyDir, file);
+                        fs.copyFile(srcPath, dstPath, (err) => {
+                            if (err) throw err;
+                        });
+                    });
+                });
             });
         });
     });
